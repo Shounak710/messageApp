cc3f8f7721eaf5a2fde311827f97b7c4482e99db
Shounak710 <shounak647@gmail.com>
2020-01-06 12:11:45 +0900

    Preventing duplicate entries in results

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :chatrooms, dependent: :destroy
+  has_and_belongs_to_many :chatrooms, dependent: :destroy -> { distinct }
   has_many :messages, dependent: :destroy
   #Validations
-   validates_presence_of :name, :email, :password_digest
-   validates :email, uniqueness: true
+   validates_presence_of :name, :password_digest
+   validates :name, uniqueness: true
  
    #encrypt password
    has_secure_password

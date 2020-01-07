fcbdeccafa1606aaa09ad14f4f9fc116f7c03d95
Shounak710 <shounak647@gmail.com>
2020-01-07 17:15:06 +0900

    Route-ine change

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
     resources :users
     post 'auth/register', to: 'users#register'
     post 'auth/login', to: 'users#login'
-    get 'test', to: 'users#test'
-    post 'message/:id', to: 'messages#send_message'
-    get 'chatroom', to: 'chatrooms#index'
-    get 'chatroom/:id', to: 'chatrooms#show' 
     get 'connect', to: 'users#connect'
+    get 'test', to: 'users#test'
+    post 'chatroom/:id', to: 'chatrooms#send_message'
+    get 'chatrooms', to: 'chatrooms#index'
+    get 'chatroom/:id', to: 'chatrooms#show' 
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

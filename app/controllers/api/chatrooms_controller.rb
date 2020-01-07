081c8cee0ade1cf074371c2d5b066492870c79fe
Shounak710 <shounak647@gmail.com>
2020-01-07 17:53:19 +0900

    Fixing route

--- app/controllers/api/chatrooms_controller.rb
+++ app/controllers/api/chatrooms_controller.rb
@@ -4,9 +4,6 @@ class Api::ChatroomsController < ApplicationController
   def index
     @chatrooms = @current_user.chatrooms
     @chatroom = []
-    # TODO: Why do you use this loop here?
-    # Can't you just return @current_user.chatrooms?
-    # Do you have a document defining your API responses?
     @chatrooms.each do |chatroom|
       c = Hash.new
       c["chatroom_id"] = chatroom.id
@@ -26,11 +23,28 @@ class Api::ChatroomsController < ApplicationController
   end
 
   def show
-    # TODO: Do you really only return the messages? Shouldn't you also return
-    # the chatroom and the participants?
-    # Again, do you have an API specification?
-    @chatroom = Chatroom.find(params[:id])
+    @chatroom = Chatroom.find(chatroom_params[:id])
     @message = @chatroom.messages
     render json: {messages: @message.to_json}
+  end
+
+  def send_message
+    # TODO: What happens if the Chatroom can not be found?
+    @chatroom = Chatroom.find(chatroom_params[:id])
+    @message = Message.create(body: params[:body], chatroom: @chatroom, sender: @current_user)
+    # TODO: When using #create, the object will automatically be saved, so no need to
+    # call save here again. Either user 'if Message.create' directly, or initialize
+    # the object with Message.new, then call #save
+    if @message.save
+      response = { message: 'Message sent' }
+      # TODO: What status does this return?
+      render json: response
+    end
+  end
+
+  private
+
+  def chatroom_params
+    params.permit(:id)
   end
 end

440b257a198806b72cf41d603f37053f5b40f557
Shounak710 <shounak647@gmail.com>
2020-01-09 10:53:07 +0900

    Removing nil return

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -11,7 +11,7 @@ class AuthorizeApiRequest
 
   def user
     @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
-    @user || errors.add(:token, 'Invalid token') && nil
+    @user || errors.add(:token, 'Invalid token')
   end
 
   def decoded_auth_token
@@ -23,6 +23,5 @@ class AuthorizeApiRequest
       return @headers['Authorization'].split(' ').last
     else errors.add(:token, 'Missing token')
     end
-    nil
   end
 end

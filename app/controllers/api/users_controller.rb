465a5da8f272c0b57d56d1706c5a10202347f4d6
Shounak710 <shounak647@gmail.com>
2020-01-10 15:46:48 +0900

    Changing response format

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -55,7 +55,7 @@ class Api::UsersController < ApplicationController
 
   def user_params
     # require user here on master branch
-    params.permit(
+    params.require(:user).permit(
       :name,
       :password
     )
